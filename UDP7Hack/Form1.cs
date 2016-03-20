e691ba9aa87eb39cd0fb32ab2eab94994211e00d
engineerhan <erhanyilmaz.ytu@gmail.com>
2016-03-20 12:08:34 +0200

    Added files via upload

---
+++ UDP7Hack/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace UDP7Hack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        int i = 0;
+        public void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(3333);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any,3333);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                //string returnData = Encoding.ASCII.GetString(receiveBytes);
+                    if (receiveBytes[0] == 0x41)
+                    {
+                        SendKeys.SendWait("{UP}");
+                    }
+                    else if (receiveBytes[0] == 0x42)
+                    {
+                        SendKeys.SendWait("{DOWN}");
+                    }
+    
+                //listBox1.Items.Add(RemoteIpEndPoint.Address.ToString() + ":" + receiveBytes[0].ToString());
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Thread thdUDPServer = new Thread(new ThreadStart(serverThread));
+            thdUDPServer.Start();
+        }
+    }
+}
+
+
